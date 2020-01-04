091ea8427ebef9a73e113b7a64f288e63c0167fe
drunkShrimper <416944223@qq.com>
2020-01-04 09:33:09 +0800

    complete get user msg

--- src/main/java/life/drunkshrimper/community/controller/AuthorizeController.java
+++ src/main/java/life/drunkshrimper/community/controller/AuthorizeController.java
@@ -1,5 +1,10 @@
 package life.drunkshrimper.community.controller;
 
+import life.drunkshrimper.community.dto.AccessTokenDTO;
+import life.drunkshrimper.community.dto.GithubUser;
+import life.drunkshrimper.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +17,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthorizeController {
 
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state")String state){
 
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
---
+++ src/main/java/life/drunkshrimper/community/provider/GithubProvider.java
@@ -0,0 +1,51 @@
+package life.drunkshrimper.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import life.drunkshrimper.community.dto.AccessTokenDTO;
+import life.drunkshrimper.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @description:
+ * @author: lipan
+ * @time: 2020/1/3 22:50
+ */
+@Component
+public class GithubProvider {
+
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+}
